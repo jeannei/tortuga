546dc6bbc3fbabf3fce64fea427c524b55b58443
Jean Nei <jeanbhs09@gmail.com>
2019-03-08 23:43:57 -0500

    feat(SymptomDetail): add tests
    * Chore restructure testing structure to provide clear separation

---
+++ server/tortuga/apps/symptoms/__tests__/base.py
@@ -0,0 +1,32 @@
+from rest_framework.test import APITestCase, APIClient
+from tortuga.apps.symptoms.models import Symptom
+
+API_VERSION_V1 = "v1"
+VERSION = "version"
+
+
+class BaseViewTest(APITestCase):
+    """
+    Scaffold symptoms
+    """
+    client = APIClient()
+
+    @staticmethod
+    def create_symptom(name="", frequency=0):
+        """
+        Create symptom
+        """
+        if name != "" and frequency > 0:
+            Symptom.objects.create(name=name, frequency=frequency)
+        else:
+            Symptom.objects.create(name=name)
+
+    def setUp(self):
+        """
+        Setup symptoms
+        """
+        self.create_symptom("sore throat")
+        self.create_symptom("itchy rash", 5)
+        self.create_symptom("runny nose", 300)
+        self.valid_symptom_id = 1
+        self.invalid_symptom_id = 100
---
+++ server/tortuga/apps/symptoms/__tests__/tests_symptoms_view.py
@@ -0,0 +1,66 @@
+"""
+Tests for Symptoms View
+"""
+from django.urls import reverse
+from rest_framework import status
+from tortuga.apps.symptoms.models import Symptom
+from tortuga.apps.symptoms.serializer import SymptomSerializer
+from tortuga.apps.symptoms.__tests__.base import BaseViewTest, API_VERSION_V1, VERSION
+
+
+SYMPTOMS_ALL = "symptoms-all"
+
+
+class SymptomsViewTest(BaseViewTest):
+    """
+    Tests for getting all symptoms
+    """
+    def test_get_all_symptoms(self):
+        """
+        This test ensures that all symptoms added in the setUp method
+        exist when we make a GET request to the symptoms/ endpoint
+        """
+        # hit the API endpoint
+        response = self.client.get(reverse(SYMPTOMS_ALL, kwargs={VERSION: API_VERSION_V1}))
+        # fetch the data from db
+        expected = Symptom.objects.all()
+        serialized = SymptomSerializer(expected, many=True)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_symptom(self):
+        """
+        This test ensures users cannot create new symptoms
+        """
+        # hit the API endpoint
+        response = self.client.post(
+            reverse(SYMPTOMS_ALL, kwargs={VERSION: API_VERSION_V1}),
+            {"name": "test 4", "frequency": 0}
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_delete_symptom(self):
+        """
+        This test ensures users cannot delete symptoms
+        """
+        # hit the API endpoint
+        response = self.client.delete(
+            reverse(SYMPTOMS_ALL, kwargs={VERSION: API_VERSION_V1}),
+            {"name": "test 4", "frequency": 0}
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_put_symptom(self):
+        """
+        This test ensures users cannot update symptoms
+        """
+        # hit the API endpoint
+        response = self.client.delete(
+            reverse(SYMPTOMS_ALL, kwargs={VERSION: API_VERSION_V1}),
+            {"name": "test 4", "frequency": 0}
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
