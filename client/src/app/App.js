c02453b4d3de830485f7244e37d25fe84d5bc025
Jean Nei <jeanbhs09@gmail.com>
2019-03-09 11:19:55 -0500

    feat(Client): init commit

---
+++ client/src/app/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import styles from './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        <header className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className={styles.link}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
