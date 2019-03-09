ed0bcbe429270cbb9d9bf2ed5fa89365ae123460
Jean Nei <jeanbhs09@gmail.com>
2019-03-08 23:43:19 -0500

    feat(SymptonDetail): add endpoint for retrieving specific symptom by id

--- server/tortuga/apps/symptoms/urls.py
+++ server/tortuga/apps/symptoms/urls.py
@@ -3,10 +3,11 @@ Symptom resources
 """
 
 from django.urls import path
-from tortuga.apps.symptoms.views import SymptomView
+from tortuga.apps.symptoms.views import SymptomsView, SymptomView
 
 # this constant must be lowercase for django to work correctly, so we tell pylint to ignore it
 # pylint: disable=C0103
 urlpatterns = [
-    path('symptoms/', SymptomView.as_view(), name="symptoms-all")
+    path('symptoms', SymptomsView.as_view(), name="symptoms-all"),
+    path('symptoms/<int:id>', SymptomView.as_view(), name="symptom-detail")
 ]
--- server/tortuga/apps/symptoms/views.py
+++ server/tortuga/apps/symptoms/views.py
@@ -3,13 +3,34 @@ API endpoints for Symptoms
 """
 
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import status
 from tortuga.apps.symptoms.models import Symptom
 from tortuga.apps.symptoms.serializer import SymptomSerializer
 
 
-class SymptomView(generics.ListCreateAPIView):
+class SymptomsView(generics.ListAPIView):
     """
-    API endpoints for Symptoms
+    GET symptoms
     """
     queryset = Symptom.objects.all()
     serializer_class = SymptomSerializer
+
+
+class SymptomView(generics.RetrieveAPIView):
+    """
+    GET symptoms/:id
+    """
+    queryset = Symptom.objects.all()
+    serializer_class = SymptomSerializer
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs["id"]
+        try:
+            a_symptom = self.queryset.get(pk=pk)
+            return Response(SymptomSerializer(a_symptom).data)
+        except Symptom.DoesNotExist:
+            return Response(
+                data={"message": "Symptom with id: {} does not exist".format(pk)},
+                status=status.HTTP_404_NOT_FOUND
+            )
